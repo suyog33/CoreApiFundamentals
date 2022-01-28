068326247f171a118959c0a64e1ceec6617e49c3
Suyog Patil <suyog33@gmail.com>
2022-01-28 15:49:55 +0530

    Module 3

---
+++ src/Data/CampProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Models;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampProfile : Profile
+    {
+        public CampProfile()
+        {
+            this.CreateMap<Camp, CampModel>()
+                .ForMember(c=>c.Venue, o=>o.MapFrom(m=>m.Location.VenueName));
+
+            this.CreateMap<Talk, TalkModel>().ReverseMap();
+
+            this.CreateMap<Speaker, SpeakerModel>().ReverseMap();
+        }
+    }
+}
